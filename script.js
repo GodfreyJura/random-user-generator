5876161ee9fb862938b37ca413e7457da4a71d12
GodfreyJura <godfreyjura382@gmail.com>
2025-08-22 15:22:34 +0300

    Add index.html, script.js and spinner.css files

---
+++ script.js
@@ -0,0 +1,65 @@
+function fetchUser() {
+  showSpinner();
+
+  fetch('https://randomuser.me/api')
+    .then((res) => res.json())
+    .then((data) => {
+      hideSpinner();
+      displayUser(data.results[0]);   
+    })
+    .catch((err) => {
+      hideSpinner();
+      console.error('Error fetching user:', err);
+    });
+}
+
+function displayUser(user) {
+  // Change background color based on gender
+  if (user.gender === 'female') {
+    document.body.style.backgroundColor = 'rebeccapurple';
+  } else {
+    document.body.style.backgroundColor = 'steelblue';
+  }
+
+  const userDisplay = document.querySelector('#user');
+
+  // Populate user info
+  userDisplay.innerHTML = `
+    <div class="flex justify-between">
+      <div class="flex">
+        <img class="w-48 h-48 rounded-full mr-8" src="${user.picture.large}" alt="${user.name.first}">
+        <div class="space-y-3">
+          <p class="text-xl">
+            <span class="font-bold">Name: </span>${user.name.first} ${user.name.last}
+          </p>
+          <p class="text-xl">
+            <span class="font-bold">Email: </span>${user.email}
+          </p>
+          <p class="text-xl">
+            <span class="font-bold">Phone: </span>${user.phone}
+          </p>
+          <p class="text-xl">
+            <span class="font-bold">Location: </span>${user.location.city}, ${user.location.country}
+          </p>
+          <p class="text-xl">
+            <span class="font-bold">Age: </span>${user.dob.age}</p>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+function showSpinner() { 
+  document.querySelector('.spinner').style.display = 'block';
+}
+
+function hideSpinner() {
+  document.querySelector('.spinner').style.display = 'none';
+}
+
+document.querySelector('#generate').addEventListener('click', fetchUser);
+
+
+fetchUser();
+
+
